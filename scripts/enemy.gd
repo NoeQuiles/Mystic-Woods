18d884fa43c564056ab77ea0c08780b4585c3e35
NoeQuiles <nqg4@alu.ua.es>
2024-10-02 20:24:18 +0200

    Combat System

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,7 +4,13 @@ var speed = 50
 var player_chase = false
 var player = null
 
+var health = 100
+var player_inattack_range = false
+var can_take_damage = true
+
 func _physics_process(delta):
+	deal_with_damage()
+	
 	if player_chase:
 		position += (player.position - position)/speed
 		$AnimatedSprite2D.play("walk")
@@ -24,3 +30,28 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
+
+func enemy():
+	pass
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		player_inattack_range = true
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		player_inattack_range = false
+
+func deal_with_damage():
+	if player_inattack_range and global.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("Slime health = ", health)
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true
